8d7e8a4715d884125a61380e20fc1dfaaa055c57
Philip Holly <philipholly@gmail.com>
2021-04-23 16:54:27 -0400

    Had to rename ConsoleApp bc of gitignore for .App suffix

---
+++ Pholly.Sample.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pholly.Sample.Shared;
+using System;
+
+namespace Pholly.Sample.App
+{
+    class Program
+    {
+        static ActivityTracker _activityTracker = new();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddAction("jump", 100);
+            AddAction("run", 75);
+            AddAction("jump", 200);
+
+            Console.WriteLine($"Avg time in seconds for all added actions: {_activityTracker.GetStats()}");
+        }
+
+        static void AddAction(string action, int timeInSeconds)
+        {
+            var result = _activityTracker.AddAction($@"{{""action"": ""{action}"", ""time"": {timeInSeconds} }}");
+            if (result == null)
+            {
+                //do something
+            }
+        }
+    }
+}
